679b555e0a9d4971958e202342a99218b3a49789
Rhcp <3076007958@qq.com>
2026-05-02 23:16:00 +0800

    完善动画脚本

--- Project/Assets/Scripts/MouseInput.cs
+++ Project/Assets/Scripts/MouseInput.cs
@@ -17,7 +17,7 @@ public class MouseInput : MonoBehaviour
     [SerializeField] private Animator animator;
     [SerializeField] private List<GameObject> foodGameObject;
 
-    public bool ISTexureNull => isTexureNull;
+    public bool ISTexureNull => isTexureNull; // 暂时没用到先放着
 
     private bool isTexureNull = true; // 判断当前玩家是否有拿着食材,true 表示当前没有拿, false 表示当前玩家有拿
     private bool isTextEnd = false; // 判断文本是否播放完毕
@@ -25,7 +25,7 @@ public class MouseInput : MonoBehaviour
     private Food food;
     private TweenerCore<string, string, DG.Tweening.Plugins.Options.StringOptions> textAnimaion;
     private Tween tween;
-    private string currentFoodName;
+    private string currentFoodName; // 用来保存当前玩家点击的食物的名字 后续用来判断动画播放和文本内容
 
     private void Update()
     {
@@ -35,36 +35,11 @@ public class MouseInput : MonoBehaviour
     private void Start()
     {
         food = FindObjectOfType<Food>();
-        animator.Play("egg");
     }
 
     // 鼠标点击检测函数
     private void Detection()
     {
-
-        if (Input.GetMouseButtonDown(0) && textUI.activeSelf && isTextEnd)
-        {
-            if (tween.IsComplete())
-            {
-                tween.Rewind();
-            }
-
-            if (textAnimaion.IsComplete())
-            {
-                textAnimaion.Rewind();
-            }
-
-            textUI.SetActive(false);
-            isTextEnd = false;
-
-        }
-        else if (Input.GetMouseButtonDown(0) && textUI.activeSelf && !isTextEnd)
-        {
-            Debug.Log("发现当前动画未完成，直接结束！");
-            textAnimaion.Complete();
-        }
-
-
         if (Input.GetMouseButtonDown(0) && !textUI.activeSelf)
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -89,9 +64,11 @@ public class MouseInput : MonoBehaviour
                 Debug.Log($"当前玩家拿着食物:{isTexureNull == false }" );
                 Debug.Log("点击的物品是：" + hit.collider.gameObject.name);
 
+                // 点击到菜板就播放做菜动画
                 if (!isTexureNull && hit.collider && hit.collider.name == "Chopping board")
                 {
                     Debug.Log(currentFoodName);
+
                     for (int i = 0; i < foodGameObject.Count; i++)
                     {
 
@@ -99,35 +76,70 @@ public class MouseInput : MonoBehaviour
                         {
                             Debug.Log("播放动画");
                             Debug.Log(foodGameObject[i].gameObject.name);
-                            StartCoroutine(PlayAnimationDelay(foodGameObject[i].gameObject.name, i));
+                            foodGameObject[i].SetActive(true);
+                            StartCoroutine(FoodAnimationAndWait(i, foodGameObject[i].gameObject.name));
                         }
                     }
                 }
             }
         }
 
-        
+        // 这里是快速播放完Dotween文字动画效果的逻辑 和 重置Dotween动画效果
+        if (Input.GetMouseButtonDown(0) && textUI.activeSelf && isTextEnd)
+        {
+            canvasGroup.alpha = 0;
+            textMeshProUGUI.text = "";
+            textUI.SetActive(false);
+            isTextEnd = false;
+
+        }
+        else if (Input.GetMouseButtonDown(0) && textUI.activeSelf && !isTextEnd)
+        {
+            Debug.Log("发现当前动画未完成，直接结束！");
+            textAnimaion.Complete();
+        }
+
     }
 
+    #region Dotween做的文字动画效果
     // 文字效果
     private void Text(string text)
     {
         tween = canvasGroup.DOFade(1, 1);
-        this.textAnimaion = DOTween.To(
-            () => "",
-            currentText => textMeshProUGUI.text = currentText,
-            text,
-            5f
-         ).SetEase(Ease.Linear).OnComplete(()=> { isTextEnd = true;  Debug.Log("文字播放完毕！"); });
+        StartCoroutine(TextAnimation(text));
     }
 
-
-    IEnumerator PlayAnimationDelay(string name, int i)
+    IEnumerator TextAnimation(string text)
     {
-        foodGameObject[i].SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        this.textAnimaion = DOTween.To(
+           () => "",
+           currentText => textMeshProUGUI.text = currentText,
+           text,
+           5f
+        ).SetEase(Ease.Linear).OnComplete(() => { isTextEnd = true; Debug.Log("文字播放完毕！"); });
+    }
+    #endregion
 
+    IEnumerator FoodAnimationAndWait(int i, string name)
+    {
+        animator.SetBool("Is" + name, true);
         yield return null;
 
-        animator.SetBool("IsActive", true);
-    }    
+        while (true)
+        {
+            AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+            Debug.Log(stateInfo.IsName(name));
+            if (stateInfo.IsName(name))
+            {
+                if (stateInfo.normalizedTime >= 1.0f)
+                    break;
+            }
+
+            yield return null;
+        }
+
+        foodGameObject[i].SetActive(false);
+        animator.SetBool("Is" + name, false);
+    }
 }
